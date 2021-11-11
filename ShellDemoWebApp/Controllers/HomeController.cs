0343f6cc918a39fbc3195992b7fef470e6f67243
H Joseph D'silva <joseph.dsilva@wipro.com>
2021-11-11 15:33:53 +0000

    Base Code

---
+++ ShellDemoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShellDemoWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String EID135592()
+        {
+            return "Hello from Joseph ver 1.0";
+        }
+    }
+}
